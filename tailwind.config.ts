506ec7c3da7425a4792fa96e2b45734cbade41fd
Pedro <pedrofigueiraboaviagem@gmail.com>
2025-03-07 23:00:00 -0300

    fix(css): fixed css variables config
    Fixed usage of css variables by adding them to the tailwindcss config file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,49 @@
 import type { Config } from 'tailwindcss'
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                background: "var(--background)",
+                foreground: "var(--foreground)",
+                card: "var(--card)",
+                cardForeground: "var(--card-foreground)",
+                popover: "var(--popover)",
+                popoverForeground: "var(--popover-foreground)",
+                primary: "var(--primary)",
+                primaryForeground: "var(--primary-foreground)",
+                secondary: "var(--secondary)",
+                secondaryForeground: "var(--secondary-foreground)",
+                muted: "var(--muted)",
+                mutedForeground: "var(--muted-foreground)",
+                accent: "var(--accent)",
+                accentForeground: "var(--accent-foreground)",
+                destructive: "var(--destructive)",
+                destructiveForeground: "var(--destructive-foreground)",
+                border: "var(--border)",
+                input: "var(--input)",
+                ring: "var(--ring)",
+                chart1: "var(--chart-1)",
+                chart2: "var(--chart-2)",
+                chart3: "var(--chart-3)",
+                chart4: "var(--chart-4)",
+                chart5: "var(--chart-5)",
+                radius: "var(--radius)",
+                sidebar: "var(--sidebar)",
+                sidebarForeground: "var(--sidebar-foreground)",
+                sidebarPrimary: "var(--sidebar-primary)",
+                sidebarPrimaryForeground: "var(--sidebar-primary-foreground)",
+                sidebarAccent: "var(--sidebar-accent)",
+                sidebarAccentForeground: "var(--sidebar-accent-foreground)",
+                sidebarBorder: "var(--sidebar-border)",
+                sidebarRing: "var(--sidebar-ring)",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 } satisfies Config
