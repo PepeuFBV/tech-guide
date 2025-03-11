02926af55c5c12adff773d1b6ba017f396b95fd9
Pedro <pedrofigueiraboaviagem@gmail.com>
2025-03-11 15:09:38 -0300

    feat(introduction): Completed ecosystem Introduction page

--- src/app/(subsection)/introduction/page.tsx
+++ src/app/(subsection)/introduction/page.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import PageIndex from '@/components/core/page-index/page-index'
 import PathDisplay from '@/components/core/path-display'
 import VisitLink from '@/components/ui/visit-link'
+import { CodeArea } from '@/components/ui/code-area'
 import { TechCard } from '@/components/ui/tech-card'
 import { Divider } from '@/components/ui/divider'
 import { NodeJS } from '@/components/ui/icons/node'
@@ -16,6 +17,7 @@ import type { Topic } from '@/types/topic'
 const topics: Topic[] = [
     { title: 'Introdução', href: 'introduction' },
     { title: 'Techstack', href: 'techstack' },
+    { title: 'Instalação', href: 'installation' },
 ]
 
 const tech: { title: string, description: string, icon?: React.ReactNode, href: string }[] = [
@@ -64,6 +66,13 @@ const tech: { title: string, description: string, icon?: React.ReactNode, href: 
 ]
 
 export default function Introduction() {
+
+    const code = [
+        `node -v
+npm -v`, `cd nextjs-react-typescript
+npm install`
+    ]
+
     return (
         <div className='w-full h-full flex items-start justify-start space-x-10'>
             <div className='md:w-[75%] lg:w-[80%] flex flex-col justify-center space-y-7 px-3 md:px-10'>
@@ -86,8 +95,25 @@ export default function Introduction() {
                         </div>
                     </div>
                     <Divider variant='horizontal' color='secondary' className='w-full' />
-                    <div>
-
+                    <div className='flex flex-col space-y-6'>
+                        <div className='flex flex-col space-y-4'>
+                            <h2 id='installation' className='text-3xl'>Instalação</h2>
+                            <p className='text-secondary'>Para mais detalhes sobre as ferramentas abaixo veja as suas respectivas seções: <VisitLink href='/node'>Node</VisitLink></p>
+                        </div>
+                        <div className='flex flex-col space-y-4'>
+                            <h3 className='text-2xl'>Node e NPM</h3>
+                            <p>Para instalar o ecossistema, você precisará ter o <VisitLink href='/node'>Node</VisitLink> instalado em sua máquina. Com o mesmo instalado, você poderá instalar as tecnologias do ecossistema através do gerenciador de pacotes npm (node package manager) ou yarn (gerenciador de pacotes alternativo). Não se preocupe, o npm é instalado automaticamente com o Node.</p>
+                            <p>Para instalar o Node.js, acesse o site oficial [<VisitLink href='https://nodejs.org/en'>aqui</VisitLink>] e baixe a versão <span className='italic'>LTS</span> (Long Term Support), está versão garante suporte a longo prazo e estabilidade. Após a instalação, você poderá verificar se o Node foi instalado corretamente através do terminal.</p>
+                        </div>
+                        <CodeArea code={code[0]} language='bash' />
+                        <div className='flex flex-col space-y-4'>
+                            <h3 className='text-2xl'>Repositório</h3>
+                            <p>Para instalar o ecossistema, você pode clonar o repositório oficial do guia através do comando abaixo:</p>
+                        </div>
+                        <CodeArea code={`git clone https://github.com/seedabit/nextjs-react-typescript.git`} language='bash' />
+                        <p>Após clonar o repositório, você pode acessar a pasta do projeto e instalar as dependências do projeto através do comando:</p>
+                        <CodeArea code={code[1]} language='bash' />
+                        <p>Agora o seu ambiente de desenvolvimento está pronto para começar a desenvolver aplicações com o ecossistema.</p>
                     </div>
                 </div>
             </div>
