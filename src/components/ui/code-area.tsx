b148723da26ca7a815847f5b3cae246bd5847382
Pedro <pedrofigueiraboaviagem@gmail.com>
2025-03-12 00:01:00 -0300

    feat(code-area): added optional title
    Added optional title for naming files/running environments

--- src/components/ui/code-area.tsx
+++ src/components/ui/code-area.tsx
@@ -9,18 +9,22 @@ const possibleLanguages = ['bash', 'json', 'typescript', 'tsx', 'css', 'html'] a
 type Language = typeof possibleLanguages[number]
 
 interface CodeAreaProps {
+    title?: string
     code: string
     language?: Language
 }
-const CodeArea: React.FC<CodeAreaProps> = ({ code, language = 'tsx' }) => {
+const CodeArea: React.FC<CodeAreaProps> = ({ title, code, language = 'tsx' }) => {
     return (
-        <div className='relative w-full'>
-            <div className='absolute p-1 right-3 top-[21px] hover:bg-zinc-800 rounded-md cursor-pointer' onClick={() => navigator.clipboard.writeText(code)}>
-                <Clipboard className='w-5 h-5 text-white' />
+        <div className='flex flex-col space-y-0'>
+            <p className='ml-1 font-thin italic'>{title}</p>
+            <div className='relative w-full'>
+                <div className='absolute p-1 right-3 top-[21px] hover:bg-zinc-800 rounded-md cursor-pointer' onClick={() => navigator.clipboard.writeText(code)}>
+                    <Clipboard className='w-5 h-5 text-white' />
+                </div>
+                <SyntaxHighlighter language={language} style={oneDark}>
+                    {code}
+                </SyntaxHighlighter>
             </div>
-            <SyntaxHighlighter language={language} style={oneDark}>
-                {code}
-            </SyntaxHighlighter>
         </div>
     )
 }
