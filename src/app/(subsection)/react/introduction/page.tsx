75e2bae53d9e044270dbf4a6bef6d9c7b1506368
Pedro <pedrofigueiraboaviagem@gmail.com>
2025-03-12 22:26:00 -0300

    fix(react-introduction): created page for build

---
+++ src/app/(subsection)/react/introduction/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Introduction() {
+    return (
+        <div>
+            <h1>Introdução</h1>
+            <p>Seja bem-vindo ao guia de desenvolvimento de templates com Next.js, React e Typescript. Aqui você aprenderá como configurar o ambiente de desenvolvimento com as melhores práticas e ferramentas do mercado.</p>
+        </div>
+    )
+}
