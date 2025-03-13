e209fed30c699faa113eaa6bcf7b2fc1661cb679
gui-henri <guihenrique.dev@gmail.com>
2025-03-13 16:33:40 -0300

    fix: correctly retrieving section and link

--- src/components/core/path-display.tsx
+++ src/components/core/path-display.tsx
@@ -4,27 +4,29 @@ import React from 'react'
 import { usePathname } from 'next/navigation'
 import Link from 'next/link'
 import { ChevronRight } from 'lucide-react'
-import { SidebarItem } from '@/types/sidebar'
+import { SidebarEntry, SidebarItem } from '@/types/sidebar'
 import { sidebar } from '@/data/sidebar'
 
 const PathDisplay = () => {
     const pathname: string = usePathname()
 
-    function getNames(pathname: string): string[] {
+    function getItem(pathname: string): SidebarItem | undefined {
         const allPaths: SidebarItem[] = sidebar.flatMap(entry => entry.items).filter(item => item !== undefined)
         const currentEntry: SidebarItem | undefined = allPaths.find(entry => entry.href === pathname)
-        return currentEntry ? [currentEntry.title] : []
+        return currentEntry;
     }
 
-    function getSection(pathname: string): SidebarItem | undefined {
-        const sidebarEntry: SidebarItem | undefined = sidebar.find(entry => entry.href === pathname)
+    function getSection(pathname: string): SidebarEntry | undefined {
+        let section = pathname.split('/')[1]
+        const sidebarEntry: SidebarEntry | undefined = sidebar.find(entry => entry.href.split('/')[1] === section)
         return sidebarEntry;
     }
 
-    const names: string[] = getNames(pathname)
+    const sectionItem = getItem(pathname)
+    const names: string[] = sectionItem ? [sectionItem.title] : [];
     if (names.length === 0) return null
     let section = getSection(pathname)
-    section = section ? section : { title: 'Ecossistema', href: '/introduction', id: 0 }
+    section = section ? section : { title: 'Ecossistema', href: '/introduction', id: 0, icon: null, items: [] }
     let currentName: string = ''
 
     return (
@@ -38,11 +40,11 @@ const PathDisplay = () => {
             )}
             <ChevronRight className='text-secondary' />
             {names.map((name, index) => {
-                currentName += `/${name}`
                 const isCurrent = index === names.length - 1
+                let currrentHref = sectionItem ? sectionItem.href : "/introduction"
                 return (
                     <React.Fragment key={index}>
-                        <Link href={currentName} className={isCurrent ? 'text-primary hover:text-secondary' : 'text-secondary'}>
+                        <Link href={currrentHref} className={isCurrent ? 'text-primary hover:text-secondary' : 'text-secondary'}>
                             {name}
                         </Link>
                         {index < names.length - 1 && <ChevronRight />}
