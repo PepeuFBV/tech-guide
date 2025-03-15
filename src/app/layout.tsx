c5b9f50514f317986ace70685123c32d7e08cf50
Pedro <pedrofigueiraboaviagem@gmail.com>
2025-03-15 16:22:04 -0300

    feat(icons): added icons
    Added icon's images and link tags in layout file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,12 @@ export default function RootLayout({
         <html lang='pt-BR'>
             <head>
                 <link rel='preload' href='/images/seed-a-bit-logo.svg' as='image' />
+                <link rel="icon" type="image/png" href="/icons/favicon-96x96.png" sizes="96x96" />
+                <link rel="icon" type="image/svg+xml" href="/icons/favicon.svg" />
+                <link rel="shortcut icon" href="/icons/favicon.ico" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
+                <meta name="apple-mobile-web-app-title" content="React Next Guide" />
+                <link rel="manifest" href="/icons/site.webmanifest" />
             </head>
             <body
                 className={`${nunito.variable} antialiased`}
