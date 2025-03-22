680eab49323f63ee89cd052c450f6ea58fc4b35d
Pedro <pedrofigueiraboaviagem@gmail.com>
2025-03-22 19:05:25 -0300

    fix: fixed page code-area components spacing

--- src/app/(subsection)/react/props/page.tsx
+++ src/app/(subsection)/react/props/page.tsx
@@ -23,11 +23,13 @@ export default function Props() {
                         <p className='text-secondary'>No React, componentes podem ser customizados de diversas formas. Uma delas são as Props. O termo vem de <i>propriedades</i> e são pequenas configurações que passamos para um componente para alterar seu comportamento. Se componentes são funções, Props são como os parâmetros dessas funções.</p>
                     </div>
                     <Divider variant='horizontal' color='secondary' className='w-full' />
-                    <div className='flex flex-col space-y-8'>
+                    <div className='flex flex-col space-y-6'>
                         <div className='flex flex-col space-y-4'>
                             <h2 id='how-to-use-props' className='text-3xl'>Como utilizar Props?</h2>
                             <p>Props são declaradas como parâmetros para as funções que criam os componentes. Simples e fáceis de executar!</p>
-                            <CodeArea link='/react/props/saudacao.tsx' />
+                        </div>
+                        <CodeArea link='/react/props/saudacao.tsx' />
+                        <div className='flex flex-col space-y-4'>
                             <p>Daqui, podemos tirar algumas observações a respeito da sintaxe:</p>
                             <ul className='list-disc list-inside space-y-2'>
                                 <li>Props são passadas para o componente como um objeto, com chaves e valores. Por isso utilizamos {"{ }"} ao redor da Prop name.</li>
@@ -35,25 +37,25 @@ export default function Props() {
                                 <li>Para adicionar uma Prop para o HTML, também é necessário colocar entre {"{ }"}.</li>
                             </ul>
                             <p>Aqui vai mais um exemplo de como podemos usar Props para alterar o funcionamento de um componente!</p>
-                            <CodeArea link='/react/props/typed-button.tsx' />
                         </div>
+                        <CodeArea link='/react/props/typed-button.tsx' />
                     </div>
                     <Divider variant='horizontal' color='secondary' className='w-full' />
                     <div className='flex flex-col space-y-6'>
                         <div className='flex flex-col space-y-4'>
                             <h2 id='types-on-props' className='text-3xl'>Tipos nas Props</h2>
                             <p>Como estamos utilizando Typescript, podemos utilizar tipos nas Props! Isso permite que quem use seu componente futuramente saiba quais atributos são obrigatórios e os tipos desses atributos. Num geral, é sempre uma boa ideia adicionar tipos as suas Props. Há várias maneiras de fazer isso, a mostrada abaixo é apenas uma delas.</p>
-                            <CodeArea link='/react/props/typed-caixa.tsx' />
                         </div>
+                        <CodeArea link='/react/props/typed-caixa.tsx' />
                     </div>
                     <Divider variant='horizontal' color='secondary' className='w-full' />
                     <div className='flex flex-col space-y-6'>
                         <div className='flex flex-col space-y-4'>
                             <h2 id='children-props' className='text-3xl'>Propriedade Children</h2>
                             <p>Dentro do HTML, somos logo de cara ensinados que um elemento {"<div></div>"} pode ter vários elementos dentro dele. Componentes não são diferentes, e quando queremos passar elementos ou texto para dentro de um componente, podemos fazer isso através da propriedade especial Children!</p>
-                            <CodeArea link='/react/props/children-button.tsx' />
-                            <p>Agora, você está pronto para aprender sobre estado!</p>
                         </div>
+                        <CodeArea link='/react/props/children-button.tsx' />
+                        <p>Agora, você está pronto para aprender sobre estado!</p>
                     </div>
                 </div>
             </div>
--- src/app/(subsection)/react/state/page.tsx
+++ src/app/(subsection)/react/state/page.tsx
@@ -22,11 +22,13 @@ export default function State() {
                         <p className='text-secondary'>Estado é quando temos uma informação ou variável que pode ser alterada com o passar do tempo. O botão de like está pressionado ou não? O que deve acontecer com meu comentário quando eu clicar em enviar? Isso tudo é resolvido utilizando estados.</p>
                     </div>
                     <Divider variant='horizontal' color='secondary' className='w-full' />
-                    <div className='flex flex-col space-y-8'>
+                    <div className='flex flex-col space-y-6'>
                         <div className='flex flex-col space-y-4'>
                             <h2 id='how-to-use-state' className='text-3xl'>Como utilizar estados?</h2>
                             <p>Estados são basicamente como cuidamos e atualizamos as variáveis dos nossos componentes. Cada componente cuida do próprio estado, então o estado de um componente não afeta o estado de outro.</p>
-                            <CodeArea link='/react/state/use-state.tsx' />
+                        </div>
+                        <CodeArea link='/react/state/use-state.tsx' />
+                        <div className='flex flex-col space-y-4'>
                             <p>No código acima, criamos um estado a partir da função useState para armazenar o valor do contador. Inicialmente, o estado começa em 0, e dois objetos são criados, <i>contador e setContador</i>. Eles funcionam como um getter e setter do Java. A variável contador serve APENAS para obter o valor do contador (como um getter). Se você quiser modificar o valor da variável, utilizamos a setContador. Quando o botão é clicado, a função setContador é chamada com contador + 1, atualizando o estado. Como o React re-renderiza o componente sempre que o estado muda, o novo valor do contador é exibido automaticamente na tela.</p>
                         </div>
                     </div>
@@ -35,7 +37,9 @@ export default function State() {
                         <div className='flex flex-col space-y-4'>
                             <h2 id='use-effect' className='text-3xl'>O useEffect e monitorando alterações no estado</h2>
                             <p>As vezes, queremos que um determinado código rode sempre que um estado for atualizado. Se o usuário faz logout, por exemplo, é interessante que no momento em que o usuário muda seu estado par "deslogado", o sistema o redirecione para outra página. Isso é realizado através da função useEffect, que sempre será executada <i>assim que o componente é mostrado em tela pela primeira vez</i> e <b>sempre que o estado for alterado por um setState</b>. Aqui podemos ver um exemplo:</p>
-                            <CodeArea link='/react/state/use-effect.tsx' />
+                        </div>
+                        <CodeArea link='/react/state/use-effect.tsx' />
+                        <div className='flex flex-col space-y-4'>
                             <p>O useEffect nesse código é usado para mudar a cor do botão sempre que o contador for atualizado (inclusive na primeira vez que o componente é montado!). Ele recebe dois argumentos: uma função de efeito e um array de dependências. Sempre que o valor de contador muda, o useEffect é disparado. Dentro dele, um array de cores é definido e a função setCor escolhe aleatoriamente uma delas para atualizar o estado. Como useEffect roda após a renderização quando contador muda, a interface do botão é re-renderizada com a nova cor.</p>
                             <p>O array [contador] no final define que esse efeito só será executado quando contador for atualizado. Isso evita execuções desnecessárias e mantém o comportamento eficiente.</p>
                         </div>
